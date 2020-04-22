c3272430cc546d4899da63652a30edbf588a7e54
Koofng <isrealakpan296@gmail.com>
2020-04-22 13:10:33 +0100

    add first GET route

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 const express = require("express");
 const app = express();
 
+const mockUserData = [
+  {
+    name: "Mark",
+  },
+  {
+    name: "Jill",
+  },
+];
+
+app.get("/users", function (req, res) {
+  res.status(200).send({
+    success: true,
+    message: "successfully got users. Nice!",
+    users: mockUserData,
+  });
+});
+
 app.listen(3000, function () {
   console.log("Server is running");
 });
