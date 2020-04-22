c1fa5d7a0f6ab543515032ec1d32b3a2a380ecb4
Koofng <isrealakpan296@gmail.com>
2020-04-22 13:42:22 +0100

    added body parser

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-app.use(bodyParser);
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
 
 const mockUserData = [
   {
